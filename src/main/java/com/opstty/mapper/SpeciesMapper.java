7c76fa25c440309cdb21a37b24286a8a9add8d67
Neptade <loic@lozolozo.com>
2024-07-15 20:05:33 +0200

    1.8.2

---
+++ src/main/java/com/opstty/mapper/SpeciesMapper.java
@@ -0,0 +1,25 @@
+package com.opstty.mapper;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class SpeciesMapper extends Mapper<Object, Text, Text, NullWritable> {
+    private Text species = new Text();
+    private boolean firstLine = true;
+
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
+        if (firstLine) {
+            firstLine = false;
+            return;
+        }
+
+        String[] attributes = value.toString().split(";");
+
+        species.set(attributes[3]);
+        context.write(species, NullWritable.get());
+    }
+}
