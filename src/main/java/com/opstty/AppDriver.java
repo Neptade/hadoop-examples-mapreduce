08175b0a5f5ab9c3eacfef28da29cd1623f70030
Neptade <loic@lozolozo.com>
2024-07-15 20:02:35 +0200

    added all possible jobs

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,13 @@
 package com.opstty;
 
 import com.opstty.job.WordCount;
+import com.opstty.job.DistrictTrees;
+import com.opstty.job.TreeSpecies;
+import com.opstty.job.SpeciesCount;
+import com.opstty.job.SpeciesMaxHeight;
+import com.opstty.job.SortHeight;
+import com.opstty.job.OldestTree;
+import com.opstty.job.DistrictMostTrees;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,6 +18,13 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("districtCount", DistrictTrees.class, "");
+            programDriver.addClass("treeSpecies", TreeSpecies.class, "");
+            programDriver.addClass("speciesCount", SpeciesCount.class, "");
+            programDriver.addClass("speciesMaxHeight", SpeciesMaxHeight.class, "");
+            programDriver.addClass("sortHeight", SortHeight.class, "");
+            programDriver.addClass("oldestTree", OldestTree.class, "");
+            programDriver.addClass("districtMostTrees", DistrictMostTrees.class, "");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
